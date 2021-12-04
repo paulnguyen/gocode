2d0a736708b5c5052ad8ca6d2caea4ab5299a91c
paulnguyen <paul.nguyen@sjsu.edu>
2021-12-03 18:59:36 -0800

    moved from cloud repo

---
+++ v1.11/gocloud/gumball/goapi/goapi_v3/src/gumball/types.go
@@ -0,0 +1,30 @@
+/*
+	Gumball API in Go (Version 3)
+	Process Order with Go Channels
+	Removed Use of Mutex
+*/
+
+package main
+
+type gumballMachine struct {
+	Id            int
+	CountGumballs int
+	ModelNumber   string
+	SerialNumber  string
+}
+
+var machine gumballMachine = gumballMachine{
+	Id:            1,
+	CountGumballs: 900,
+	ModelNumber:   "M102988",
+	SerialNumber:  "1234998871109",
+}
+
+type order struct {
+	Id          string
+	OrderStatus string
+}
+
+var orders map[string]order
+var order_queue = make(chan string, 10)
+var order_update = make(chan int, 1000)
